e8534f81323c1fefc0b36d428007d561de4e2cc8
Luca Giampieri <luca.giampieri95@gmail.com>
2026-02-06 14:37:11 +0100

    Aggiunta rotte  "/posts"  con annessi GET, POST, PUT, PATCH e DELETE

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const postsRouter = require('./routes/posts');
 
 app.use(express.static('public'));
 
@@ -10,6 +12,7 @@ app.get('/', (req, res) => {
 
 })
 
+app.use("/posts", postsRouter);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
